87e0e542abe7c7ee1d9ceab149d8486536499f04
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2024-01-23 17:44:54 -0700

    first card

--- app/(menu)/dashboard/page.js
+++ app/(menu)/dashboard/page.js
@@ -1,17 +1,16 @@
-"use client"
+"use client";
 
-import { AppStateProvider } from '../../global-state/AppStateContext';
+import { AppStateProvider } from "../../global-state/AppStateContext";
 import Charts from "@/app/components/Charts";
+import { DashboardCard } from "@/app/components/DashboardCard";
 import RecentAppointments from "@/app/components/RecentAppointments";
 
 const Dashboard = () => {
-
   return (
     <AppStateProvider>
       <div className="col-span-4 w-full h-full p-8 mt-20">
-        <h2 className="text-[#2D4635] font-bold text-xl">
-          Dashboard
-        </h2>
+        <h2 className="text-[#2D4635] font-bold text-xl">Dashboard</h2>
+        <DashboardCard />
         <Charts />
         <RecentAppointments />
       </div>
