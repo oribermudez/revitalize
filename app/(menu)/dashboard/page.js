ea776d64ff6814bcdde9f135d2970b65a93285e9
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2024-01-25 09:39:41 -0700

    Changed card color and shape

--- app/(menu)/dashboard/page.js
+++ app/(menu)/dashboard/page.js
@@ -5,12 +5,22 @@ import Charts from "@/app/components/Charts";
 import { DashboardCard } from "@/app/components/DashboardCard";
 import RecentAppointments from "@/app/components/RecentAppointments";
 
+const cardDataArray = [
+  { title: "Cancellations", value: 5, unit: "/week" },
+  { title: "Appointments", value: 45, unit: "/week" },
+  { title: "Patients", value: 50, unit: "/week" },
+];
+
 const Dashboard = () => {
   return (
     <AppStateProvider>
       <div className="col-span-4 w-full h-full p-8 mt-20">
         <h2 className="text-[#2D4635] font-bold text-xl">Dashboard</h2>
-        <DashboardCard />
+        <div className="flex flex-wrap gap-4">
+          {cardDataArray.map((cardDataContent, index) => (
+            <DashboardCard key={index} cardData={cardDataContent} />
+          ))}
+        </div>
         <Charts />
         <RecentAppointments />
       </div>
--- app/components/DashboardCard.js
+++ app/components/DashboardCard.js
@@ -8,39 +8,44 @@ import {
   Button,
 } from "@material-tailwind/react";
 
-export function DashboardCard() {
+export function DashboardCard({ cardData }) {
   return (
-    <Card color="black" variant="gradient" className="w-full max-w-[20rem] p-8">
-      <CardHeader
-        floated={false}
-        shadow={false}
-        color="transparent"
-        className="m-0 mb-8 rounded-none border-b border-white/10 pb-8 text-center">
-        <Typography
-          variant="small"
-          color="black"
-          className="font-normal uppercase">
-          Cancellations
-        </Typography>
-        <Typography
-          variant="h1"
-          color="black"
-          className="mt-6 flex justify-center gap-1 text-7xl font-normal">
-          <span className="mt-2 text-4xl"></span>5{" "}
-          <span className="self-end text-4xl">/week</span>
-        </Typography>
-      </CardHeader>
-      <CardBody className="p-0"></CardBody>
-      <CardFooter className="mt-12 p-0">
-        <Button
-          size="lg"
-          color="white"
-          className="hover:scale-[1.02] focus:scale-[1.02] active:scale-100"
-          ripple={false}
-          fullWidth={true}>
-          View All
-        </Button>
-      </CardFooter>
-    </Card>
+    <>
+      {cardData && (
+        <Card
+          variant="gradient"
+          className="w-full  bg-green-200 max-w-[20rem] p-8 rounded-full  flex items-center">
+          <CardHeader
+            floated={false}
+            shadow={false}
+            color="transparent"
+            className="m-0 mb-8 rounded-none border-b border-white/10 pb-8 text-center">
+            <Typography
+              variant="small"
+              color="black"
+              className="font-normal uppercase">
+              {cardData.title}
+            </Typography>
+            <Typography
+              variant="h1"
+              color="black"
+              className="mt-6 flex justify-center gap-1 text-7xl font-normal">
+              <span className="mt-2 text-4xl"></span>
+              {cardData.value}{" "}
+              <span className="self-end text-4xl">{cardData.unit}</span>
+            </Typography>
+          </CardHeader>
+          <CardBody className="p-0"></CardBody>
+          <CardFooter className="mt-5 p-0">
+            <Button
+              size="lg"
+              className="underline underline-offset-1 text-gray-500 hover:scale-[1.02] focus:scale-[1.02] active:scale-100 w-50"
+              ripple={true}>
+              Details
+            </Button>
+          </CardFooter>
+        </Card>
+      )}
+    </>
   );
 }
