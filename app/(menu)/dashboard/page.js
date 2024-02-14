e3f58ba1bc036b10d53474868a2b74ed1ddc9fba
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2024-02-13 20:57:06 -0700

    Changes in responsive design

--- app/(menu)/dashboard/page.js
+++ app/(menu)/dashboard/page.js
@@ -11,17 +11,23 @@ const Dashboard = () => {
     <AppStateProvider>
       <div className="col-span-4 w-full h-full p-8 mt-20 ">
         <h2 className="text-[#2D4635] font-bold text-xl">Dashboard</h2>
-        <div className="grid grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {/* First column taking up 3 */}
-          <div className="col-start-1 col-end-4">
+          <div className="col-span-3 md:col-start-1 md:col-end-4">
             <DashboardCard />
           </div>
-          <div className="col-start-1 col-end-3">
-            <RecentAppointments />
-          </div>
-          <div className="col-span-1 col-end-4">
-            <PatientsVisitsChart />
-            <ServicesChart />
+          {/* Second column taking up 3 */}
+          <div className="md:col-span-3 md:col-start-1 md:col-end-4">
+            <div className="col-span-3 md:col-start-1 md:col-end-3">
+              <RecentAppointments />
+            </div>
+            <div className="col-span-3 md:col-span-1 md:col-end-4">
+              <PatientsVisitsChart />
+            </div>
+
+            <div className="col-span-3 md:col-span-1 md:col-end-4">
+              <ServicesChart />
+            </div>
           </div>
         </div>
       </div>
