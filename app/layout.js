2c0fa59b6d83bc498dba840e514913c19c511598
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2024-02-26 16:41:42 -0700

    Added app state provider to app layout

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { AppStateProvider } from "./global-state/AppStateContext";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Revitalize | Massage Therapy',
-  description: 'Massage therapy for the whole family',
-}
+  title: "Revitalize | Massage Therapy",
+  description: "Massage therapy for the whole family",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <AppStateProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </AppStateProvider>
+  );
 }
