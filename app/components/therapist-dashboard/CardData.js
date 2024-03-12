9544e5b6ce5995e27cd1866240fbc445234b3d37
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2024-03-11 21:30:03 -0600

    Finished fetching data dashboard cards

--- app/components/therapist-dashboard/CardData.js
+++ app/components/therapist-dashboard/CardData.js
@@ -1,59 +1,25 @@
 import { useState, useEffect } from "react";
-import {
-  UsersIcon,
-  ClipboardDocumentListIcon,
-  ArchiveBoxXMarkIcon,
-  BanknotesIcon,
-} from "@heroicons/react/24/outline";
 
-export function CardData() {
-  const [cardData, setCardData] = useState([]);
-
+export function CardData({ onDataFetch }) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/appointments");
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const appointments = await response.json();
+        const salesData = await calculateSalesForCurrentMonth();
+        const appointmentsCountData = await monthAppointmentsCount();
+        const cancelledAppointmentsData =
+          await calculateCancelledAppointmentsPercentage();
+        const frequentClientsData = await calculateFrequentClients();
 
-        // Calculate metrics based on fetched data
-        const sales = await calculateSales(); // Call calculateSales function
-        const totalAppointments = await monthAppointmentsCount(); // Call appointmentsCount function
-        const cancelledAppointments = await cancelledAppointmentsPercentage();
-        const frequentClients = await calculateFrequentClients();
-
-        setCardData([
-          {
-            title: "Frequent Clients",
-            value: 250,
-            monthlyGoal: 65,
-            backgroundColor: "main",
-            icon: UsersIcon,
-          },
-          {
-            title: "Total Appointments",
-            value: totalAppointments,
-            monthlyGoal: 44,
-            backgroundColor: "secondary",
-            icon: ClipboardDocumentListIcon,
-          },
-          {
-            title: "Cancellations",
-            value: cancelledAppointments,
-            monthlyGoal: 10,
-            backgroundColor: "accent",
-            icon: ArchiveBoxXMarkIcon,
-          },
-          {
-            title: "Sales",
-            value: sales,
-            monthlyGoal: 30,
-            backgroundColor: "secondary",
-            icon: BanknotesIcon,
-          },
-        ]);
+        onDataFetch({
+          sales: salesData.total,
+          salesPercentage: salesData.percentage,
+          appointmentsCount: appointmentsCountData.count,
+          appointmentsPercentage: appointmentsCountData.percentage,
+          cancelledAppointmentsCount: cancelledAppointmentsData.count,
+          cancelledAppointmentsPercentage: cancelledAppointmentsData.percentage,
+          frequentClients: frequentClientsData.count,
+          frequentClientsPercentage: frequentClientsData.percentage,
+        });
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -62,169 +28,176 @@ export function CardData() {
     fetchData();
   }, []);
 
-  async function calculateSales() {
+  async function calculateSalesForCurrentMonth() {
     try {
+      // Get the current date
+      const currentDate = new Date();
+      const currentMonth = currentDate.getMonth() + 1; // Adding 1 to get the month in the range 1-12
+      const currentYear = currentDate.getFullYear();
+
+      // Fetch the invoices data
       const response = await fetch("/api/invoices");
       if (!response.ok) {
         throw new Error("Failed to fetch data");
       }
       const invoices = await response.json();
 
-      // Calculate the total sales for the current month
-      const totalSales = invoices.reduce((total, invoice) => {
+      // Filter invoices for the current month and year
+      const filteredInvoices = invoices.filter((invoice) => {
+        const transactionDate = new Date(invoice.transactionDate);
+
+        return (
+          transactionDate.getMonth() === currentMonth - 1 && // JavaScript months are zero-based
+          transactionDate.getFullYear() === currentYear
+        );
+      });
+
+      // Calculate the total sales for the filtered invoices
+      const totalSales = filteredInvoices.reduce((total, invoice) => {
         return total + invoice.totalCost;
       }, 0);
-      return totalSales;
+
+      //calculate the percentage of sales compared to last 30 days
+      const last30Days = new Date();
+      last30Days.setDate(last30Days.getDate() - 30);
+      const last30DaysInvoices = invoices.filter((invoice) => {
+        const transactionDate = new Date(invoice.transactionDate);
+        return transactionDate >= last30Days;
+      });
+      const last30DaysSales = last30DaysInvoices.reduce((total, invoice) => {
+        return total + invoice.totalCost;
+      }, 0);
+      const percentage = Math.round((totalSales / last30DaysSales) * 100);
+
+      return {
+        total: totalSales,
+        percentage: percentage,
+      };
     } catch (error) {
       console.error("Error calculating sales:", error);
       return 0;
     }
   }
 
-  return cardData;
-}
+  async function monthAppointmentsCount() {
+    try {
+      const currentDate = new Date();
+      const currentMonth = currentDate.getMonth() + 1; // Adding 1 to get the month in the range 1-12
+      const currentYear = currentDate.getFullYear();
 
-// Export calculateSales function for use in other components
-// Define a function to calculate sales for the current month
-export async function calculateSalesForCurrentMonth() {
-  try {
-    // Get the current date
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth() + 1; // Adding 1 to get the month in the range 1-12
-    const currentYear = currentDate.getFullYear();
-
-    //console.log("Current month:", currentMonth);
-
-    // Fetch the invoices data
-    const response = await fetch("/api/invoices");
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const invoices = await response.json();
-    //console.log("All invoices:", invoices);
-
-    // Filter invoices for the current month and year
-    const filteredInvoices = invoices.filter((invoice) => {
-      const transactionDate = new Date(invoice.transactionDate);
-      //console.log("Transaction date:", transactionDate);
-      return (
-        transactionDate.getMonth() === currentMonth - 1 && // JavaScript months are zero-based
-        transactionDate.getFullYear() === currentYear
-      );
-    });
-    //console.log("Filtered invoices:", filteredInvoices);
-
-    // Calculate the total sales for the filtered invoices
-    const totalSales = filteredInvoices.reduce((total, invoice) => {
-      return total + invoice.totalCost;
-    }, 0);
-
-    return totalSales;
-  } catch (error) {
-    console.error("Error calculating sales:", error);
-    return 0;
-  }
-}
-
-//Total number of appointments for the current month
-export async function monthAppointmentsCount() {
-  try {
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth() + 1; // Adding 1 to get the month in the range 1-12
-    const currentYear = currentDate.getFullYear();
-
-    const response = await fetch("/api/appointments");
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const appointments = await response.json();
-    //console.log("All appointments:", appointments);
-
-    // Filter appointments for the current month and year
-    const filteredAppointments = appointments.filter((appointment) => {
-      const appointmentDate = new Date(appointment.startTime);
-      return (
-        appointmentDate.getMonth() === currentMonth - 1 && // JavaScript months are zero-based
-        appointmentDate.getFullYear() === currentYear
-      );
-    });
-    console.log("current month:", currentMonth);
-
-    // Return the length of filtered appointments
-    return filteredAppointments.length;
-  } catch (error) {
-    console.error("Error fetching appointments:", error);
-    return 0;
-  }
-}
-
-//count of cancelled appointments percentage of total appointments of the month
-export async function cancelledAppointmentsPercentage() {
-  try {
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth() + 1; // Adding 1 to get the month in the range 1-12
-    const currentYear = currentDate.getFullYear();
-
-    const response = await fetch("/api/appointments");
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const appointments = await response.json();
-
-    // Filter appointments for the current month and year
-    const filteredAppointments = appointments.filter((appointment) => {
-      const appointmentDate = new Date(appointment.startTime);
-      return (
-        appointmentDate.getMonth() === currentMonth - 1 && // JavaScript months are zero-based
-        appointmentDate.getFullYear() === currentYear
-      );
-    });
-    console.log("current month hereeeee:", currentMonth);
-    console.log("filtered appointments:", filteredAppointments);
-    const cancelledAppointments = filteredAppointments.filter(
-      (appointment) => appointment.status === "cancelled"
-    );
-    console.log("cancelled appointments:", cancelledAppointments);
-
-    // Return the length of filtered appointments
-    return (cancelledAppointments.length / filteredAppointments.length) * 100;
-  } catch (error) {
-    console.error("Error fetching appointments:", error);
-    return 0;
-  }
-}
-
-export async function calculateFrequentClients() {
-  try {
-    const response = await fetch("/api/clients");
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const clients = await response.json();
-
-    // Filter clients with appointments
-    const clientsWithAppt = clients.filter((client) => {
-      if (client.appointments) {
-        const appointments = Object.values(client.appointments);
-        return appointments.length > 1; // Filter clients with more than one appointment
+      const response = await fetch("/api/appointments");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
       }
-      return false; // If client has no appointments, return false
-    });
+      const appointments = await response.json();
 
-    // Calculate the percentage of clients with more than one appointment compared to the total number of clients
-    const totalClients = clients.length;
-    const frequentClientsCount = clientsWithAppt.length;
-    const percentage = (frequentClientsCount / totalClients) * 100;
+      // Filter appointments for the current month and year
+      const filteredAppointments = appointments.filter((appointment) => {
+        const appointmentDate = new Date(appointment.startTime);
+        return (
+          appointmentDate.getMonth() === currentMonth - 1 && // JavaScript months are zero-based
+          appointmentDate.getFullYear() === currentYear
+        );
+      });
 
-    return {
-      count: frequentClientsCount,
-      percentage: percentage.toFixed(2), // Round percentage to two decimal places
-    };
-  } catch (error) {
-    console.error("Error calculating frequent clients:", error);
-    return {
-      count: 0,
-      percentage: 0,
-    };
+      const appointmentsCount = filteredAppointments.length;
+
+      //calculate the percentage of appointments compared to last 30 days
+      const last30Days = new Date();
+      last30Days.setDate(last30Days.getDate() - 30);
+      const last30DaysAppointments = appointments.filter((appointment) => {
+        const appointmentDate = new Date(appointment.startTime);
+        return appointmentDate >= last30Days;
+      });
+      const last30DaysAppointmentsCount = last30DaysAppointments.length;
+      const percentage = Math.round(
+        (appointmentsCount / last30DaysAppointmentsCount) * 100
+      );
+
+      return {
+        count: appointmentsCount,
+        percentage: percentage,
+      };
+    } catch (error) {
+      console.error("Error fetching appointments:", error);
+      return 0;
+    }
   }
+
+  async function calculateCancelledAppointmentsPercentage() {
+    try {
+      const currentDate = new Date();
+      const currentMonth = currentDate.getMonth() + 1; // Adding 1 to get the month in the range 1-12
+      const currentYear = currentDate.getFullYear();
+
+      const response = await fetch("/api/appointments");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const appointments = await response.json();
+
+      // Filter appointments for the current month and year
+      const filteredAppointments = appointments.filter((appointment) => {
+        const appointmentDate = new Date(appointment.startTime);
+        return (
+          appointmentDate.getMonth() === currentMonth - 1 && // JavaScript months are zero-based
+          appointmentDate.getFullYear() === currentYear
+        );
+      });
+
+      const cancelledAppointments = filteredAppointments.filter(
+        (appointment) => appointment.status === "cancelled"
+      );
+
+      // Return the length of filtered appointments and the percentage
+      const cancelledPercentage =
+        (cancelledAppointments.length / filteredAppointments.length) * 100;
+      const cancelledAppointmentsCount = cancelledAppointments.length;
+      return {
+        count: cancelledAppointmentsCount,
+        percentage: cancelledPercentage,
+      };
+    } catch (error) {
+      console.error("Error fetching appointments:", error);
+      return 0;
+    }
+  }
+
+  async function calculateFrequentClients() {
+    try {
+      const response = await fetch("/api/clients");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const clients = await response.json();
+
+      // Filter clients with appointments
+      const clientsWithAppt = clients.filter((client) => {
+        if (client.appointments) {
+          const appointments = Object.values(client.appointments);
+          return appointments.length > 1; // Filter clients with more than one appointment
+        }
+        return false; // If client has no appointments, return false
+      });
+
+      // Calculate the percentage of clients with more than one appointment compared to the total number of clients
+      const totalClients = clients.length;
+      const frequentClientsCount = clientsWithAppt.length;
+      const frequentClientsPercentage =
+        (frequentClientsCount / totalClients) * 100;
+
+      return {
+        count: frequentClientsCount,
+        percentage: frequentClientsPercentage, // Round percentage to two decimal places
+      };
+    } catch (error) {
+      console.error("Error calculating frequent clients:", error);
+      return {
+        count: 0,
+        percentage: 0,
+      };
+    }
+  }
+
+  return null;
 }
--- app/components/therapist-dashboard/DashboardCard.js
+++ app/components/therapist-dashboard/DashboardCard.js
@@ -8,6 +8,12 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
+import {
+  UsersIcon,
+  ClipboardDocumentListIcon,
+  ArchiveBoxXMarkIcon,
+  BanknotesIcon,
+} from "@heroicons/react/24/outline";
 
 import {
   CircularProgressbarWithChildren,
@@ -15,92 +21,107 @@ import {
 } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import { CardData } from "./CardData";
-import {
-  calculateSalesForCurrentMonth,
-  monthAppointmentsCount,
-  cancelledAppointmentsPercentage,
-} from "./CardData";
 
 export function DashboardCard() {
-  const cardData = CardData();
-  const [sales, setSales] = useState(0);
-  const [appointmentsCount, setAppointmentsCount] = useState(0);
+  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchSales = async () => {
-      try {
-        const fetchedSales = await calculateSalesForCurrentMonth();
-        setSales(fetchedSales);
-
-        const fetchedAppointmentsCount = await monthAppointmentsCount();
-        setAppointmentsCount(fetchedAppointmentsCount);
-      } catch (error) {
-        console.error("Error fetching sales:", error);
-      }
-    };
-
-    fetchSales();
-  }, []);
+  const onDataFetch = (fetchedData) => {
+    setData(fetchedData);
+  };
 
   return (
     <>
-      <div className="flex flex-wrap justify-between ">
-        {cardData.map((item, index) => (
-          <div
-            key={index}
-            className="w-full lg:w-1/3 xl:w-1/4 mb-8 max-w-[20rem]">
-            <Card
-              variant="gradient"
-              className={`w-full lg:w-auto p-4 max-w-full shadow-2xl rounded-2xl bg-${item.backgroundColor} transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300`}
-              style={{ height: "200px" }}>
-              <CardHeader
-                floated={false}
-                shadow={false}
-                color="transparent"
-                className="mt-1  mr-3 text-center flex justify-end items-end ">
-                <item.icon className="h-6 w-6 mr-2 text-white " />
-              </CardHeader>
-              <CardBody className="flex justify-between p-0">
-                <div style={{ width: 116, height: 116 }}>
-                  <CircularProgressbarWithChildren
-                    value={item.value}
-                    text={`${item.monthlyGoal}%`}
-                    background
-                    styles={buildStyles({
-                      textColor: "#ffff",
-                      pathTransitionDuration: 0.15,
-                      trailColor: "#e4e4e7",
-                      backgroundColor: "transparent",
-                      pathColor: "#ffff",
-                    })}
-                    className="ml-6"
-                  />
-                </div>
-                <Typography
-                  variant="h1"
-                  color="black"
-                  className=" gap-1 text-4xl m-5 font-bold text-white ">
-                  {item.title === "Sales"
-                    ? sales
-                    : item.value === "Total Appointments"
-                    ? appointmentsCount
-                    : item.value === "Cancellations"
-                    ? cancelledAppointmentsPercentage
-                    : item.value}
-                </Typography>
-              </CardBody>
-              <CardFooter className="ml-8 p-0">
-                <Button
-                  size="lg"
-                  className="hover:bg-gray-100 font-bold uppercase text-white shadow-transparent  w-50"
-                  ripple={true}>
-                  {item.title}
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-        ))}
-      </div>
+      <CardData onDataFetch={onDataFetch} />
+      {data && (
+        <div className="flex flex-wrap justify-between ">
+          {[
+            {
+              title: "Frequent Clients",
+              value: data.frequentClients,
+              percentage: data.frequentClientsPercentage,
+              backgroundColor: "main",
+              icon: UsersIcon,
+            },
+            {
+              title: "Total Appointments",
+              value: data.appointmentsCount,
+              percentage: data.appointmentsPercentage,
+              backgroundColor: "secondary",
+              icon: ClipboardDocumentListIcon,
+            },
+            {
+              title: "Cancellations",
+              value: data.cancelledAppointmentsCount,
+              percentage: data.cancelledAppointmentsPercentage,
+              backgroundColor: "accent",
+              icon: ArchiveBoxXMarkIcon,
+            },
+            {
+              title: "Sales",
+              value: data.sales,
+              percentage: data.salesPercentage,
+              backgroundColor: "secondary",
+              icon: BanknotesIcon,
+            },
+          ].map((item, index) => (
+            <div
+              key={index}
+              className="w-full lg:w-1/3 xl:w-1/4 mb-8 max-w-[20rem]">
+              <Card
+                variant="gradient"
+                className={`w-full lg:w-auto p-4 max-w-full shadow-2xl rounded-2xl bg-${item.backgroundColor} transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300`}
+                style={{ height: "200px" }}>
+                <CardHeader
+                  floated={false}
+                  shadow={false}
+                  color="transparent"
+                  className="mt-1  mr-3 text-center flex justify-end items-end ">
+                  <item.icon className="h-6 w-6 mr-2 text-white " />
+                </CardHeader>
+                <CardBody className="flex justify-between p-0">
+                  <div style={{ width: 116, height: 116 }}>
+                    <CircularProgressbarWithChildren
+                      value={item.value}
+                      text={`${item.percentage}%`}
+                      background
+                      styles={buildStyles({
+                        textColor: "#ffff",
+                        pathTransitionDuration: 0.15,
+                        trailColor: "#e4e4e7",
+                        backgroundColor: "transparent",
+                        pathColor: "#ffff",
+                      })}
+                      className="ml-6"
+                    />
+                  </div>
+                  <Typography
+                    variant="h1"
+                    color="black"
+                    className=" gap-1 text-4xl m-5 font-bold text-white ">
+                    {item.title === "Sales"
+                      ? item.value
+                      : item.title === "Total Appointments"
+                      ? item.value
+                      : item.title === "Cancellations"
+                      ? item.value
+                      : item.title === "Frequent Clients"
+                      ? item.value
+                      : item.value}
+                  </Typography>
+                </CardBody>
+                <CardFooter className="ml-8 p-0">
+                  <Button
+                    size="lg"
+                    className="hover:bg-gray-100 font-bold uppercase text-white shadow-transparent  w-50"
+                    ripple={true}>
+                    {item.title}
+                  </Button>
+                </CardFooter>
+              </Card>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
